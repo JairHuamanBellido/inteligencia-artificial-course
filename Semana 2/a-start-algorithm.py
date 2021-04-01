185b2c1c08ed674ea08083f195aa088d296e888b
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2021-04-01 11:13:52 -0500

    Update a start algorithm

--- Semana 2/a-start-algorithm.py
+++ Semana 2/a-start-algorithm.py
@@ -1,8 +1,8 @@
 # F(n) = G(n) + H(n)
-
+# Author: Jair Orlando Huaman Bellido
 class Path:
     def __init__(self, cost, node):
-        self.g      = cost
+        self.cost   = cost
         self.node   = node
 
 class Node: 
@@ -14,7 +14,7 @@ class Node:
         self.name   = name
 
     def __lt__(self,other):
-        return self.h < other.h
+        return self.f < other.f
     
     def asignPath(self, nodeTarget, cost):
         path = Path(cost,nodeTarget)
@@ -37,33 +37,47 @@ def findPath(nodeOrigin,nodeTarget):
 
     # Repetir hasta abiertos es vacio o encontrado
     while len(openNodes) > 0:
-        
+
         # Buscar el mejor con la F mínima (Ordenar) [1,2,3,4]
         openNodes.sort()
 
         # Extraer el menor Indice[0] -> MEJOR NODO
         bestNode =  openNodes.pop(0)
-        
+
         # Mover el nodo a los cerrados
         closedNodes.append(bestNode)
 
         # Si el mejor nodo es igual a NodeTarget
         if bestNode == nodeTarget:
-            print("Se ha encontrado")
             break
 
         paths = bestNode.path
 
         for path in paths:
+            # Encontrar el mejor costo
+            current_cost = bestNode.g + path.cost
 
+            # Si se encuentra el hijo en cerrados
             if path.node in openNodes == True:
-                if path.g <  
+                # Si el camino actual es menor a valor G del vecino
+                if current_cost <  path.node.g:
+                    path.node.g =  current_cost
+                    path.node.f = path.node.g + path.node.h
+                    openNodes.append(path.node)
                 continue
+            # Si se encuentra el hijo en cerrados
             if path.node in closedNodes == True:
+                # Si el camino actual es menor a valor G del vecino
+                if current_cost <  path.node.g:
+                    path.node.g =  current_cost
+                    path.node.f = path.node.g + path.node.h
+                    openNodes.append(path.node)
                 continue
             else:
-                openNodes.append(path.node)     
-                path.node.f = path.g + path.node.h
+                path.node.g = current_cost     
+                path.node.f = path.node.g + path.node.h
+                openNodes.append(path.node)
+    return closedNodes    
 
 
 
@@ -93,5 +107,10 @@ nodeC.asignPath(nodeTarget,4)
 # Nodo D
 nodeD.asignPath(nodeTarget,2)
 
+# EXCERSISES
+# https://www.youtube.com/watch?v=PzEWHH2v3TE&t=33s
 
-# findPath(nodeS,nodeTarget)
+print("La ruta es: ")
+for path in findPath(nodeS,nodeTarget):
+    print(path.name)
+    
